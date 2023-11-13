3a67edda99720000d6581ed2326d468e08209510
AlexandreDresch <z3r0pc@protonmail.com>
2023-11-13 15:57:45 -0300

    feat: add footer component

---
+++ src/components/footer/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+  display: flex;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  background-color: #eeeeee;
+
+  p {
+    padding: 8px 0px 8px 0px;
+    font-family: Montserrat;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 15px;
+    letter-spacing: 0em;
+    text-align: left;
+  }
+`;
